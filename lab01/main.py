e2607685fc0a6da98a4a26b088120da51d8e6543
Professor Laney <dena.laney@bellevuecollege.edu>
2026-07-05 22:09:36 -0700

    Add initial comments and instructions to main.py

---
+++ lab01/main.py
@@ -0,0 +1,10 @@
+# DEV 108 Lab Activity 1
+# [Today's Date]
+# [Your Name]
+
+# Start writing your code below.
+# Click the "Run" button (or run this file in the terminal with: python main.py)
+# to see your output in the terminal window.
+# Good luck! You can do this - please don't use any AI tools to do this assignment. 
+# That violates the AI Policy for this class and puts your learning at a disadvantage. 
+
